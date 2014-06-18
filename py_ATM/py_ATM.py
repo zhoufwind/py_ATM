4a3a9aa3f37de25d5c418b58af2195799680f4db
Stephen Zhou <zhoufwind@hotmail.com>
2014-06-17 20:28:14 -0700

    Add function: init()

--- py_ATM/py_ATM.py
+++ py_ATM/py_ATM.py
@@ -1,1 +1,17 @@
-print('Hello World')
+
+def init():
+    _account = {}
+    with open('account.txt') as f:
+        f.readline()    # Ingore first line -- the comment bar
+        rename = 0      # If account name repeat, using 'rename' variable change duplicate name
+        for i in f.readlines():
+            line = i.strip().split()
+            while _account.has_key(line[0]):    # If account name repeat, adding '-n' at endings, 'rename' varibale decide 'n'
+                print "User exist!!"
+                line[0] += '-' + str(rename)
+                rename += 1
+                continue
+            _account[line[0]] = line[1:]        # Adding account information into dictionary
+    print _account
+
+init()
