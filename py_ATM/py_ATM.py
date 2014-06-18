f42622a6b389b803a0576f817f4ef554bb6ba593
Stephen Zhou <zhoufwind@hotmail.com>
2014-06-17 21:20:41 -0700

    Adding function: checkBlock(), write(), login(), complete login function

--- py_ATM/py_ATM.py
+++ py_ATM/py_ATM.py
@@ -1,6 +1,8 @@
 
 _account = {}
 _account_file = 'account.txt'
+_block_file = 'blockList.txt'
+_times = 3
 
 def read():
     "Import account file into memory, creating account dictionary"
@@ -21,4 +23,41 @@ def lookup(n):
     "Check if user account exist in the dictionary, if exist, return user's information or 'True'"
     return _account.get(n)
 
+def checkBlock(name):
+    "Chcked if user account exist in block List"
+    with open(_block_file) as f:
+        for line in f.readlines():
+            if name == line.strip('\n'):
+                return True
+                break
+            else:
+                continue
+
+def write(n):
+    f = open(_block_file, 'a')
+    f.writelines(n + '\n')        # Windows using \r\n
+    f.close()
+
+def login(t):
+    "Login function, first check if account name in the account dictionary, then check if user in blocklist, then begin login process, block user if login failed t times"
+    name = raw_input('Enter account name: ')
+    if lookup(name):                # Check if user exist in the account
+        # print "Found the user!", lookup(name)
+        if not checkBlock(name):    # Check if user have been blocked
+            for i in range(t):      # Verify the account name and password within 't' times
+                pwd = raw_input('Enter password: ')
+                if pwd == _account[name][0]:
+                    print 'Login success!!\nWelcome!!'
+                    break
+                else:
+                    print 'Password incorrect!'
+            else:
+                print 'Login Failed %s times, block this account!!' % t
+                write(name)         # Adding user into block list
+        else:
+            print 'User have been blocked already!'
+    else:
+        print "User isn't exist!!"
+
 read()
+login(_times)
