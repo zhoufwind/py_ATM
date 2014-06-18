25a743d4b0b6f192f1cc8ed169f4c6d2d6ecdf9a
Stephen Zhou <zhoufwind@hotmail.com>
2014-06-17 20:51:18 -0700

    Adding function: lookup(), modified init() to read()

--- py_ATM/py_ATM.py
+++ py_ATM/py_ATM.py
@@ -1,17 +1,24 @@
 
-def init():
-    _account = {}
-    with open('account.txt') as f:
+_account = {}
+_account_file = 'account.txt'
+
+def read():
+    "Import account file into memory, creating account dictionary"
+    with open(_account_file) as f:
         f.readline()    # Ingore first line -- the comment bar
         rename = 0      # If account name repeat, using 'rename' variable change duplicate name
         for i in f.readlines():
             line = i.strip().split()
-            while _account.has_key(line[0]):    # If account name repeat, adding '-n' at endings, 'rename' varibale decide 'n'
-                print "User exist!!"
+            while lookup(line[0]):    # If account name repeat, adding '-n' at endings, 'rename' varibale decide 'n'
+                print "User %s exist!!" % line[0]
                 line[0] += '-' + str(rename)
                 rename += 1
                 continue
             _account[line[0]] = line[1:]        # Adding account information into dictionary
     print _account
 
-init()
+def lookup(n):
+    "Check if user account exist in the dictionary, if exist, return user's information or 'True'"
+    return _account.get(n)
+
+read()
