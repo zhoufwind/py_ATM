caac8ccd308ac39374a22ed98cbe9a6dc971e7ba
Stephen Zhou <zhoufwind@hotmail.com>
2014-06-18 22:41:38 -0700

    Adding Module: pickle, R/W binary file with dictionary

--- py_ATM/py_ATM.py
+++ py_ATM/py_ATM.py
@@ -1,9 +1,11 @@
 import time
+import pickle
 
 _accounts = {}
 _account_file = 'account.txt'
 _block_file = 'blockList.txt'
 _log_file = 'credit_account.log'
+_account_pickle = 'account.pickle'
 
 def read():
     "Import account file into memory, creating account dictionary"
@@ -71,44 +73,66 @@ def loger(account, tran_date, tran_type, amount, interest):
     f.close()
 
 def record_amount(account, tran_date, tran_type, amount, interest):
-    balance = int(_accounts[account][2])
-    if amount < 0:
+    balance = int(_entry[account][2])
+    if amount < 0:      # Deposit when amount less than 0
         if _tran_type == 'cash':
             balance -= amount
+            _entry[_account][2] = balance   # Update binary file(_account_pickle) -- deposit
+            with open (_account_pickle, 'wb') as f:
+                pickle.dump(_entry, f)
             print "Cash In: %s by %s success!" % (-amount, tran_type)
             print "You have %s rest now!" % balance
             loger(account, tran_date, tran_type, amount, interest)
         else:
             print "You must deposit by cash!"
-    else:
-        if balance < amount:
-            print "Sorry, you don't have enough money to buy it!!"
-        else:
-            balance -= (amount + interest)
-            print "Pay Out: %s + Interest: %s by %s success!" % (amount, interest, tran_type)
-            print "You have %s rest now!" % balance
-            loger(account, tran_date, tran_type, amount, interest)
+    else:               # Withdrawal when amount greater than 0
+        if _tran_type == 'cash':    # Withdrawal by cash, will cost interest
+            interest = 0.05 * int(_amount)        # set interest
+            if balance < (amount + interest):
+                print "Amount: %s + Interest: %s = %s" % (amount, interest, amount + interest)
+                print "Sorry, you don't have enough money to buy it!!"
+            else:
+                balance -= (amount + interest)
+                _entry[_account][2] = balance   # Update binary file(_account_pickle) -- withdrawal by cash
+                with open (_account_pickle, 'wb') as f:
+                    pickle.dump(_entry, f)
+                print "Pay Out: %s + Interest: %s by %s success!" % (amount, interest, tran_type)
+                print "You have %s rest now!" % balance
+                loger(account, tran_date, tran_type, amount, interest)
+        else:                       # Withdrawal by card, no interest
+            if balance < amount:
+                print "Sorry, you don't have enough money to buy it!!"
+            else:
+                balance -= amount
+                _entry[_account][2] = balance   # Update binary file(_account_pickle) -- withdrawal by card
+                with open (_account_pickle, 'wb') as f:
+                    pickle.dump(_entry, f)
+                print "Pay Out: %s + Interest: %s by %s success!" % (amount, interest, tran_type)
+                print "You have %s rest now!" % balance
+                loger(account, tran_date, tran_type, amount, interest)
 
 read()          # Import account information into app
 _times = 3      # block accunt if login failed 3 times
 
 print "***Welcome to ATM***"
+with open (_account_pickle, 'wb') as f:     # dump dictionary(_accounts) to binary file(_account_pickle)
+    pickle.dump(_accounts, f)
+with open (_account_pickle, 'rb') as f:     # load binary file(_account_pickle) into dictionary(_entry)
+    _entry = pickle.load(f)
 while True:
     _account = raw_input('Enter account name: ')    # Specific user login
     if len(_account) == 0: continue
     if login(_account, _times):         # If user login success, he can continue following functions
         print "***Main Window***"
         while True:
+
             _tran_date = time.strftime('%Y-%m-%d/%H:%M:%S', time.localtime())           # Log local time as transaction time
-            _balance = int(_accounts[_account][2])
-            print "You have %s totally!" % _balance
+            _balance = int(_entry[_account][2])
+            print "You have %s in total!" % _balance
             _tran_type = raw_input("Transcation type('Card' or 'Cash'?): ").strip().lower()
             if len(_tran_type) == 0: continue
             _amount = raw_input("Transaction amount: ").strip()
-            if _tran_type == 'cash':
-                _interest = 0.05 * int(_amount)        # set interest
-            else:
-                _interest = 0
+            _interest = 0
             record_amount(_account, _tran_date, _tran_type, int(_amount), _interest)
             if raw_input("----------------------\n>>>Enter any key to continue...<<<\n>>>Enter 'q' to logout...<<<\n") == 'q':
                 break
