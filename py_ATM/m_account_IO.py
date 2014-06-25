eef9489b3ca12119db83bf427ab0ff7fd0a7b1bf
Stephen Zhou <zhoufwind@hotmail.com>
2014-06-24 19:33:32 -0700

    Update: ATM main function only using pickle file directly, do not read/write origin account file every time bow

--- py_ATM/m_account_IO.py
+++ py_ATM/m_account_IO.py
@@ -8,21 +8,21 @@ _account_pickle = 'account.pickle'      # define at m_record_amount as well
 
 def readAccount():
     "Import account file into memory, creating account dictionary"
-    accounts = {}
-    with open(_account_file) as f:
-        #f.readline()    # Ingore first line -- the comment bar
-        rename = 0      # If account name repeat, using 'rename' variable change duplicate name
-        for i in f.readlines():
-            line = i.strip().split()
-            while m_login.f_lookup(accounts, line[0]):    # If account name repeat, adding '-n' at endings, 'rename' varibale decide 'n'
-                #print "Account %s exist!!" % line[0]
-                line[0] += '-' + str(rename)
-                #print "Modify it to: %s\n----------------------" % line[0]
-                rename += 1
-                continue
-            accounts[line[0]] = line[1:]        # Adding account information into dictionary
-    with open (_account_pickle, 'wb') as f:         # dump dictionary(_accounts) to binary file(_account_pickle)
-        pickle.dump(accounts, f)
+    #accounts = {}
+    #with open(_account_file) as f:
+    #    #f.readline()    # Ingore first line -- the comment bar
+    #    rename = 0      # If account name repeat, using 'rename' variable change duplicate name
+    #    for i in f.readlines():
+    #        line = i.strip().split()
+    #        while m_login.f_lookup(accounts, line[0]):    # If account name repeat, adding '-n' at endings, 'rename' varibale decide 'n'
+    #            #print "Account %s exist!!" % line[0]
+    #            line[0] += '-' + str(rename)
+    #            #print "Modify it to: %s\n----------------------" % line[0]
+    #            rename += 1
+    #            continue
+    #        accounts[line[0]] = line[1:]        # Adding account information into dictionary
+    #with open (_account_pickle, 'wb') as f:         # dump dictionary(_accounts) to binary file(_account_pickle)
+    #    pickle.dump(accounts, f)
     with open (_account_pickle, 'rb') as f:         # load binary file(_account_pickle) into dictionary(_entry)
         entry = pickle.load(f)
     #print accounts
